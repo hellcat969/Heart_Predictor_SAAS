19cfd8adbce801744365d06ece2f2c6c8ff65a2f
sarmad <csarmada328@gmail.com>
2025-02-03 16:20:18 +0500

    gan

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './COMPONENTS/BANNER/Main';
-import Footer from './COMPONENTS/FOOTER/Footer';
-import Navbar from './COMPONENTS/NAVBAR/Navbar';
-import Blog from './COMPONENTS/BLOG/Blog';
-import BlogDetail from './COMPONENTS/BLOG/BLOGDETAILS/Blogdetails'; // Import the BlogDetail component
-import PrivacyPolicy from './COMPONENTS/PRIVACYPolicy/Privacypolicy';
-import AboutUs from './COMPONENTS/ABOUTUS/AboutUs';
-import Predict from './COMPONENTS/PREDICTOR/Predict';
-import Pricing from './COMPONENTS/PRICING/Pricing';
+import Main from "./COMPONENTS/BANNER/Main";
+import Footer from "./COMPONENTS/FOOTER/Footer";
+import Navbar from "./COMPONENTS/NAVBAR/Navbar";
+import Blog from "./COMPONENTS/BLOG/Blog";
+import BlogDetail from "./COMPONENTS/BLOG/BLOGDETAILS/Blogdetails";
+import PrivacyPolicy from "./COMPONENTS/PRIVACYPolicy/Privacypolicy";
+import AboutUs from "./COMPONENTS/ABOUTUS/AboutUs";
+import Predict from "./COMPONENTS/PREDICTOR/Predict";
+import Pricing from "./COMPONENTS/PRICING/Pricing";
+
+// Initialize Google Analytics
+ReactGA.initialize("G-7HBRLGXP1K"); // Replace with your Measurement ID
+
+// Page View Tracking Component
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+};
 
 function App() {
   return (
     <>
       <Router>
-
+        <Analytics /> {/* Google Analytics Tracking */}
         <Navbar />
         <Routes>
           <Route path="/" element={<Main />} />
@@ -24,8 +45,7 @@ function App() {
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/p" element={<Predict />} />
-          <Route path="/p" element={<Predict />} />
-          <Route path="/pr" element={<Pricing/>} />
+          <Route path="/pr" element={<Pricing />} />
         </Routes>
         <Footer />
       </Router>
