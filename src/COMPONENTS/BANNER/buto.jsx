2950e5733224bb3a5540390d5ef382cbd236f9ca
sarmad <csarmada328@gmail.com>
2025-02-07 09:57:05 +0500

    fvg

--- src/COMPONENTS/BANNER/buto.jsx
+++ src/COMPONENTS/BANNER/buto.jsx
@@ -7,7 +7,7 @@ const SupportOpenSource = () => {
       <div className="support-content">
         <h2 className="support-title">API For Developers</h2>
         <p className="support-description">
-        The Heart Attack Risk Prediction API is a powerful tool designed to assist developers in building applications focused on health analytics and preventive care. This API leverages advanced machine learning models to predict the risk of a heart attack based on user-provided health parameters. It provides reliable and accurate results, enabling developers to create innovative and life-saving solutions for end users.
+        The Heart Attack Risk Calculator API is a powerful tool designed to assist developers in building applications focused on health analytics and preventive care. This API leverages advanced machine learning models to predict the risk of a heart attack based on user-provided health parameters. It provides reliable and accurate results, enabling developers to create innovative and life-saving solutions for end users.
         </p>
       </div>
       <div className="support-button">
